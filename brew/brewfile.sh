e5f8c1132670a465d49e72dcf3712566bd97bea2
hyr3k <rhrkf.1021@gmail.com>
2015-05-20 05:00:06 +0900

    added peco

---
+++ brew/brewfile.sh
@@ -0,0 +1,31 @@
+brew tap caskroom/homebrew-cask
+brew tap peco/peco
+
+brew install brew-cask
+brew install git
+brew install zsh
+brew install vim --with-lua
+brew install wget
+brew install ssh-copy-id
+brew install tmux
+brew install sl
+brew install tree
+brew install peco
+
+brew cask install alfred
+brew cask install coteditor
+brew cask install dropbox
+brew cask install vlc
+brew cask install xquartz
+brew cask install yorufukurou
+brew cask install iterm2
+brew cask install limechat
+brew cask install kobito
+brew cask install google-japanese-ime
+brew cask install google-chrome
+brew cask install firefox
+brew cask install evernote
+brew cask install atom
+brew cask install tunnelbrick
+
+brew cask alfred link
