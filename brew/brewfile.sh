3d699113e6c6b6038665061947dc2c5f45c77e97
Hiroki Yoshimura <kadokusei@users.noreply.github.com>
2018-10-11 10:58:43 +0900

    fix brewfile

--- brew/brewfile.sh
+++ brew/brewfile.sh
@@ -1,32 +1,27 @@
-brew tap caskroom/homebrew-cask
-brew tap peco/peco
-
 brew install brew-cask
 brew install git
 brew install zsh
-brew install vim --with-lua
 brew install wget
 brew install ssh-copy-id
 brew install tmux
 brew install sl
 brew install tree
-brew install peco
+brew install reattach-to-user-namespace
+brew install python
 
 brew cask install alfred
 brew cask install coteditor
 brew cask install dropbox
 brew cask install vlc
-brew cask install xquartz
 brew cask install yorufukurou
 brew cask install iterm2
-brew cask install limechat
-brew cask install kobito
 brew cask install google-japanese-ime
-brew cask install google-chrome
+brew cask install google-chrome-beta
 brew cask install firefox
 brew cask install evernote
 brew cask install atom
 brew cask install tunnelbrick
 brew cask install gyazo
+brew cask install visual-studio-code
 
 brew cask alfred link
